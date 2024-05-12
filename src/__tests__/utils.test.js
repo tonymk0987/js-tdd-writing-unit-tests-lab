7bfe79669dcf544783903fad21e36231ac698f14
tonymk0987 <k.tony.muriuki@gmail.com>
2024-05-12 21:06:54 +0300

    Done

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,10 @@
 // Your tests here
+const { isPalindrome } = require('../utils');
+
+test('returns true for a palindrome', () => {
+  expect(isPalindrome("racecar")).toBe(true);
+});
+
+test('returns false for a non-palindrome', () => {
+  expect(isPalindrome("car")).toBe(false);
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,10 @@
 // Your code here
+function isPalindrome(word) {
+    const lowercaseWord = word.toLowerCase();
+    if (lowercaseWord.length === 0 || !/^[a-z]+$/.test(lowercaseWord)) {
+      throw new Error('Input must be a non-empty string containing only alphabetic characters');
+    }
+    return lowercaseWord === lowercaseWord.split('').reverse().join('');
+  }
+  module.exports = { isPalindrome };
+  
